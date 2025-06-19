bdac7833e6e2e557a652044febb155a50d7328e7
AhmedAyman77 <ai388981@gmail.com>
2025-06-19 19:32:37 +0300

    Fix Dockerfile and dependencies

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,9 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Install spaCy model
+RUN python -m spacy download en_core_web_sm
+
 COPY . .
 
 EXPOSE 7860
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from routes import ImageSearch, TextSearch
 from transformers import CLIPProcessor, CLIPModel
 
 app = FastAPI()
-load_dotenv()
+# load_dotenv()
 
 # ================================================================
 # The @app.on_event("startup") is a decorator that specifies a function
@@ -21,8 +21,8 @@ load_dotenv()
 @app.on_event("startup")
 async def startup__span():
     # Load the Feature Extraction model
-    # MODEL_ID = "openai/clip-vit-base-patch32"
-    MODEL_ID = "./models/clip_model"
+    MODEL_ID = "openai/clip-vit-base-patch32"
+    # MODEL_ID = "./models/clip_model"
     app.model = CLIPModel.from_pretrained(MODEL_ID)
     app.processor = CLIPProcessor.from_pretrained(MODEL_ID)
     
